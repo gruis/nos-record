52fa4650d869558d2315ca5b17913db70d8bde84
Caleb Crane <github@simulacre.org>
2013-06-19 20:13:37 +0900

    remove stupid spec data & clear out tmp leveldb

--- spec/connection_leveldb_spec.rb
+++ spec/connection_leveldb_spec.rb
@@ -4,6 +4,9 @@ describe "GemVault::Model::Connection::LevelDB" do
   let(:path) do
     File.expand_path("../tmp/spec.ldb", __FILE__)
   end
+  after(:all) do
+    FileUtils.rm_rf path
+  end
 
   context "#new" do
     let(:connection) do
