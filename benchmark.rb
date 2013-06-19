b0302b349dddbd5d327209f3ea9b704cc25673ed
Caleb Crane <github@simulacre.org>
2013-06-19 21:37:02 +0900

    benchmark uses larger dataset & sqlite in memory

--- benchmark.rb
+++ benchmark.rb
@@ -5,35 +5,41 @@ require "bundler"
 Bundler.setup
 require "gem-vault/server/user"
 require "gem-vault/model/connection"
+require "securerandom"
+
+def report_per_sec(tms, n, space)
+  per_sec = tms.real < 1 ? ((1/tms.real) * n) : n / tms.real
+  puts "#{space} #{per_sec.round.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")}/sec"
+end
 
 level_path  = File.expand_path("../tmp/bench.ldb", __FILE__)
 sqlite_path = File.expand_path("../tmp/bench.sqlite", __FILE__)
 kyoto_path  = File.expand_path("../tmp/bench.kch", __FILE__)
 
-leveldb    = GemVault::Model::Connection::LevelDB.new(level_path)
-sqlite     = GemVault::Model::Connection::Sqlite.new(sqlite_path)
-redis      = GemVault::Model::Connection::Redis.new
-redisock   = GemVault::Model::Connection::Redis.new(:path => "/tmp/redis.sock")
-kcb        = GemVault::Model::Connection::KyotoCabinet.new(kyoto_path)
-user       = GemVault::Server::User.new(:id => "benchmark", :email => "benchmark@gemvau.lt")
-
 begin
   n       = 5_000
   n_human = "#{n / 1000}k"
+
+  rand_users = n.times.map { |i| GemVault::Server::User.new(:id => i.to_s, :email => "bench.#{i}@gemvau.lt") }
+  user_ids   = []
+  user       = GemVault::Server::User.new(:id => "benchmark", :email => "benchmark@gemvau.lt")
+
+  leveldb        = GemVault::Model::Connection::LevelDB.new(level_path)
+  sqlite_memory  = GemVault::Model::Connection::Sqlite.new ":memory:"
+  sqlite         = GemVault::Model::Connection::Sqlite.new(sqlite_path)
+  redis          = GemVault::Model::Connection::Redis.new
+  redisock       = GemVault::Model::Connection::Redis.new(:path => "/tmp/redis.sock")
+  kcb            = GemVault::Model::Connection::KyotoCabinet.new(kyoto_path)
+
 
   to_test = [
     ["KyotoCabinet", kcb],
     ["LevelDB", leveldb],
     ["Redis", redis],
     ["Redis (Unix Socket)", redis],
+    ["SQLite (Memory)", sqlite_memory],
     ["SQLite", sqlite]
   ]
-
-
-  def report_per_sec(tms, n, space)
-    per_sec = tms.real < 1 ? ((1/tms.real) * n) : n / tms.real
-    puts "#{space} #{per_sec.round.to_s.gsub(/(\d)(?=(\d\d\d)+(?!\d))/, "\\1,")}/sec"
-  end
 
   to_test.each do |name, con|
     $stderr.puts "\n\n#{name}"
@@ -49,20 +55,24 @@ begin
     rw_time = nil
 
     Benchmark.bm(20) do |x|
-      r_time = x.report("(#{n_human}) read:") do
-        n.times { con.get(key, GemVault::Server::User) }
-      end
-      report_per_sec(r_time, n, ("    "))
-
       w_time = x.report("(#{n_human}) write:") do
-        n.times { con.save(user) }
+        n.times do |i|
+          user_ids << con.save(rand_users[i])
+        end
       end
       report_per_sec(w_time, n, ("    "))
 
+      r_time = x.report("(#{n_human}) read:") do
+        n.times do |i|
+          con.get(rand_users[i].id, GemVault::Server::User)
+        end
+      end
+      report_per_sec(r_time, n, ("    "))
+
       rw_time = x.report("(#{n_human}) read & write:") do
         n.times do |i|
-          con.get(key, GemVault::Server::User)
-          con.save(user) if n % 10
+          con.get(rand_users[i].id, GemVault::Server::User)
+          con.save(rand_users[i]) if i % 10
         end
       end
       report_per_sec(rw_time, n, ("    "))
@@ -74,53 +84,69 @@ ensure
 end
 
 =begin
+ * 2.6Ghz Intel Core i7
+ * 16GB 1600 MHz DDr3
+ * APPLE SSD SM512E
+ * Ruby 2.0.0p195 (2013-05-14 revision 40734) [x86_64-darwin12.4.0]
+
 
 KyotoCabinet
                            user     system      total        real
-(5k) read:             0.030000   0.000000   0.030000 (  0.033991)
-     147,097/sec
-(5k) write:            0.050000   0.000000   0.050000 (  0.055352)
-     90,331/sec
-(5k) read & write:     0.070000   0.010000   0.080000 (  0.072730)
-     68,748/sec
+(25k) write:           0.200000   0.010000   0.210000 (  0.204523)
+     122,236/sec
+(25k) read:            0.220000   0.000000   0.220000 (  0.230515)
+     108,453/sec
+(25k) read & write:    0.460000   0.010000   0.470000 (  0.461851)
+     54,130/sec
 
 
 LevelDB
                            user     system      total        real
-(5k) read:             0.030000   0.000000   0.030000 (  0.032942)
-     151,782/sec
-(5k) write:            0.050000   0.000000   0.050000 (  0.057885)
-     86,378/sec
-(5k) read & write:     0.090000   0.000000   0.090000 (  0.086949)
-     57,505/sec
+(25k) write:           0.210000   0.010000   0.220000 (  0.216882)
+     115,270/sec
+(25k) read:            0.230000   0.000000   0.230000 (  0.233257)
+     107,178/sec
+(25k) read & write:    0.520000   0.010000   0.530000 (  0.504035)
+     49,600/sec
 
 
 Redis
                            user     system      total        real
-(5k) read:             0.420000   0.100000   0.520000 (  0.536349)
-     9,322/sec
-(5k) write:            0.390000   0.080000   0.470000 (  0.500218)
-     9,996/sec
-(5k) read & write:     0.800000   0.170000   0.970000 (  1.005320)
-     4,974/sec
+(25k) write:           1.410000   0.420000   1.830000 (  2.064798)
+     12,108/sec
+(25k) read:            1.470000   0.430000   1.900000 (  2.070735)
+     12,073/sec
+(25k) read & write:    2.880000   0.840000   3.720000 (  4.121688)
+     6,065/sec
 
 
 Redis (Unix Socket)
                            user     system      total        real
-(5k) read:             0.410000   0.090000   0.500000 (  0.509919)
-     9,805/sec
-(5k) write:            0.400000   0.080000   0.480000 (  0.495219)
-     10,097/sec
-(5k) read & write:     0.810000   0.160000   0.970000 (  1.009409)
-     4,953/sec
+(25k) write:           1.360000   0.410000   1.770000 (  1.959406)
+     12,759/sec
+(25k) read:            1.490000   0.390000   1.880000 (  2.126249)
+     11,758/sec
+(25k) read & write:    2.850000   0.770000   3.620000 (  4.085881)
+     6,119/sec
+
+
+SQLite (Memory)
+                           user     system      total        real
+(25k) write:           0.970000   0.010000   0.980000 (  0.971103)
+     25,744/sec
+(25k) read:            0.960000   0.000000   0.960000 (  0.969800)
+     25,779/sec
+(25k) read & write:    2.090000   0.010000   2.100000 (  2.106009)
+     11,871/sec
 
 
 SQLite
                            user     system      total        real
-(5k) read:             0.220000   0.080000   0.300000 (  0.300650)
-     16,631/sec
-(5k) write:            0.470000   1.520000   1.990000 (  3.549052)
-     1,409/sec
-(5k) read & write:     0.790000   1.600000   2.390000 (  3.976819)
-     1,257/sec
+(25k) write:           2.290000   7.760000  10.050000 ( 17.473671)
+     1,431/sec
+(25k) read:            1.210000   0.370000   1.580000 (  1.578052)
+     15,842/sec
+(25k) read & write:    4.020000   8.560000  12.580000 ( 21.033940)
+     1,189/sec
+
 =end
